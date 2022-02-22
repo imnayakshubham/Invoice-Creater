a77ce57c5a2b0c0e0764cdb7b756766ca71a9b4e
Akash <akashpatel18041999@gmail.com>
2022-02-22 19:15:51 +0530

    feat : added prettier

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,20 +6,7 @@ export const AuthContext = createContext();
 
 export const useAuth = () => {
   useContext(AuthContext);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 export const AuthProvider = ({ children }) => {
   const [loading, setloading] = useState(true);
