64743702fea3c65af47b9af410bdfcdd4f59f727
Akash <akashpatel18041999@gmail.com>
2022-02-27 16:23:30 +0530

    feat : added invoices screen

--- src/components/HomeScreen/HomeScreen.js
+++ src/components/HomeScreen/HomeScreen.js
@@ -5,9 +5,8 @@ import Header from "../Header/Header";
 import Invoice from "../Invoice/Invoice";
 import PrivateRoute from "../PrivateRoute";
 import Profile from "../Profile/Profile";
-import ViewInvoice from "../ViewInvoice/ViewInvoice";
+import Invoices from "../Invoices/Invoices";
 import Welcome from "../Welcome/Welcome";
-// import Profile from '../Profile/Profile'
 import CreateInvoice from "./../CreateInvoice/CreateInvoice";
 
 function HomeScreen() {
@@ -18,7 +17,7 @@ function HomeScreen() {
         <hr />
         <Switch>
           <Route exact path="/" component={Welcome} />
-          <PrivateRoute exact path="/invoices" component={ViewInvoice} />
+          <PrivateRoute exact path="/invoices" component={Invoices} />
           <PrivateRoute exact path="/invoice/:id" component={Invoice} />
           <PrivateRoute exact path="/profile" component={Profile} />
           <PrivateRoute exact path="/create" component={CreateInvoice} />
---
+++ src/components/Invoices/Invoices.js
@@ -0,0 +1,14 @@
+import { Container } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+
+export default function ({}) {
+  const [invoices, setInvoices] = useState([]);
+
+  const getInvoices = async () => {
+    const res = await db;
+  };
+
+  useEffect(() => {}, []);
+
+  return <Container fixed></Container>;
+}
